cea470ca7952a950a139e6c8cbc961e8f063568c
Qui Le <levanqui88@gmail.com>
2019-05-22 06:51:05 +0700

    * update

--- app.js
+++ app.js
@@ -18,20 +18,19 @@ app.get('/', function(req, res) {
     var urlToScreenshot = parseUrl(req.query.url);
 
     if (validUrl.isWebUri(urlToScreenshot)) {
-        console.log('Screenshotting: ' + urlToScreenshot);
-        var scheduePost = schedule.scheduleJob('5 * * * * *',async function(){
-            var currentTime = new Date();
-            console.log("####### CurrentTime2 :: " + currentTime);
-        });
-
         (async() => {
             const browser = await puppeteer.launch({
                 args: ['--no-sandbox', '--disable-setuid-sandbox']
             });
 
             const page = await browser.newPage();
+            
+            await page.setViewport({ width: 1280, height: 800 });
             await page.goto(urlToScreenshot);
-            await page.screenshot().then(function(buffer) {
+            
+            await page.waitFor(1000);
+
+            await page.screenshot({fullPage: true }).then(function(buffer) {
                 res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
                 res.setHeader('Content-Type', 'image/png');
                 res.send(buffer)
